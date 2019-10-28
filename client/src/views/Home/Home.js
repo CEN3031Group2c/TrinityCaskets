da907c9273fd9be216f48ae24fd4f32d5969da3d
acomiskey <acomiskey@ufl.edu>
2019-10-27 20:08:58 -0400

    Cleared document for later.

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -6,17 +6,7 @@ function Home() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                </a>
+                future TrinityCaskets website.
             </header>
         </div>
     );
