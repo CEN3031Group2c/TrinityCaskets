b2b85e8184255049555f9e8d02033df63315135a
Bao Nguyen <nguyenbao@ufl.edu>
2019-10-29 09:19:28 -0400

    restructure template

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -7,20 +7,7 @@ function Home() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <p>This will be our home page</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                </a>
-=======
-                future TrinityCaskets website.
->>>>>>> 267f9ab9878641a9f4c9ff3ef9fa2db0482e3708
+                <p>future TrinityCaskets website.</p>
             </header>
         </div>
     );
