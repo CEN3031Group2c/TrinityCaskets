b3a576db53dbb1475ad782c63ba44786dff803b0
mdelsole <mdelsole64@gmail.com>
2019-11-01 17:46:53 -0400

    Implemented actions for authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component} from 'react';
 import { Route, Switch, Redirect  } from 'react-router-dom';
 import Home from "./views/Home/Home"
 import NotFound from "./views/NotFound"
@@ -9,22 +9,29 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import { loadUser } from './redux/actions/authActions';
 
-// Added the store for redux
-const App = () => {
-    return (
-        <Provider store={store}>
-            <div>
-                <Header />
-                <Switch>
-                    <Route exact path="/Home" component={Home} />
-                    <Route exact path="/">
-                        <Redirect to="/Home" />
-                    </Route>
-                    <Route component={NotFound}/>
-                </Switch>
-            </div>
-        </Provider>
-    );
-};
+class App extends Component {
+    // Dispatch the 'check if user logged in'
+    componentDidMount() {
+        store.dispatch(loadUser());
+    }
+
+    // Added the store for redux. The rest is the same as before
+    render() {
+        return (
+            <Provider store={store}>
+                <div>
+                    <Header/>
+                    <Switch>
+                        <Route exact path="/Home" component={Home}/>
+                        <Route exact path="/">
+                            <Redirect to="/Home"/>
+                        </Route>
+                        <Route component={NotFound}/>
+                    </Switch>
+                </div>
+            </Provider>
+        );
+    };
+}
 
 export default App;
