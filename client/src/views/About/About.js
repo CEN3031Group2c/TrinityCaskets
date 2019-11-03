d0e138abaed05cc7daf7ec48d1becb40f9ee1e42
Josequintas96 <josequintas@ufl.edu>
2019-11-03 12:36:57 -0500

    a

---
+++ client/src/views/About/About.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from '../../assets/logo.svg';
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <p>This will be our home page</p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                </a>
+=======
+                future TrinityCaskets website.
+>>>>>>> 267f9ab9878641a9f4c9ff3ef9fa2db0482e3708
+            </header>
+        </div>
+    );
+}
+
+export default Home;
